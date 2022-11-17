bf92b8bb39993192735221f678ba9a90f477f497
Matt Rajkowski <sourcecoder@zeroio.com>
2022-11-16 19:48:47 -0500

    Latest release

--- src/main/java/com/simisinc/platform/ApplicationInfo.java
+++ src/main/java/com/simisinc/platform/ApplicationInfo.java
@@ -32,7 +32,7 @@ public class ApplicationInfo {
   // Use: Change the date, increment the decimal on same day updates
   // then reset back to 10000
   //                         VERSION = "--------.10000";
-  public static final String VERSION = "20221116.10000";
+  public static final String VERSION = "20221116.10001";
 
   /**
    * Outputs the version from the command line
