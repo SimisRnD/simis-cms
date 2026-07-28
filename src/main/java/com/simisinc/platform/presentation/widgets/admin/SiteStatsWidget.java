35dac87a5d22b4c9d83f2a9b155c1bd797023a2e
Elizabeth Houser <lizhsr@users.noreply.github.com>
2026-07-28 13:20:13 -0700

    Mailing list metrics: replace the inflated Total Sign-ups counter with real distinct-subscriber counts

--- src/main/java/com/simisinc/platform/presentation/widgets/admin/SiteStatsWidget.java
+++ src/main/java/com/simisinc/platform/presentation/widgets/admin/SiteStatsWidget.java
@@ -24,6 +24,7 @@ import com.simisinc.platform.infrastructure.persistence.SessionRepository;
 import com.simisinc.platform.infrastructure.persistence.UserRepository;
 import com.simisinc.platform.infrastructure.persistence.cms.WebPageHitRepository;
 import com.simisinc.platform.infrastructure.persistence.cms.WebSearchRepository;
+import com.simisinc.platform.infrastructure.persistence.mailinglists.MailingListMemberRepository;
 import com.simisinc.platform.infrastructure.persistence.login.UserLoginRepository;
 import com.simisinc.platform.presentation.widgets.GenericWidget;
 import com.simisinc.platform.presentation.widgets.cms.PreferenceEntriesList;
@@ -163,6 +164,22 @@ public class SiteStatsWidget extends GenericWidget {
       Long totalUserCount = UserRepository.countTotalUsers();
       context.getRequest().setAttribute("numberValue", String.valueOf(totalUserCount));
       return CARD_JSP;
+    } else if ("active-mailing-list-subscribers".equalsIgnoreCase(report)) {
+      long count = MailingListMemberRepository.countActiveSubscribers();
+      context.getRequest().setAttribute("numberValue", String.valueOf(count));
+      return CARD_JSP;
+    } else if ("unsubscribed-mailing-list-members".equalsIgnoreCase(report)) {
+      long count = MailingListMemberRepository.countUnsubscribed();
+      context.getRequest().setAttribute("numberValue", String.valueOf(count));
+      return CARD_JSP;
+    } else if ("monthly-mailing-list-subscriptions".equalsIgnoreCase(report)) {
+      List<StatisticsData> statisticsDataList = MailingListMemberRepository.findMonthlySubscriptions(12);
+      context.getRequest().setAttribute("statisticsDataList", statisticsDataList);
+      return JSP;
+    } else if ("daily-mailing-list-subscriptions".equalsIgnoreCase(report)) {
+      List<StatisticsData> statisticsDataList = MailingListMemberRepository.findDailySubscriptions(30);
+      context.getRequest().setAttribute("statisticsDataList", statisticsDataList);
+      return JSP;
     } else if ("daily-hits".equalsIgnoreCase(report)) {
       List<StatisticsData> statisticsDataList = WebPageHitRepository.findDailyWebHits(30);
       context.getRequest().setAttribute("statisticsDataList", statisticsDataList);
--- src/test/java/com/simisinc/platform/presentation/widgets/admin/SiteStatsWidgetTest.java
+++ src/test/java/com/simisinc/platform/presentation/widgets/admin/SiteStatsWidgetTest.java
@@ -17,11 +17,16 @@
 package com.simisinc.platform.presentation.widgets.admin;
 
 import com.simisinc.platform.WidgetBase;
+import com.simisinc.platform.domain.model.dashboard.StatisticsData;
 import com.simisinc.platform.infrastructure.persistence.SessionRepository;
+import com.simisinc.platform.infrastructure.persistence.mailinglists.MailingListMemberRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mockStatic;
 
 /**
@@ -29,6 +34,90 @@ import static org.mockito.Mockito.mockStatic;
  * @created 5/9/2022 7:00 AM
  */
 class SiteStatsWidgetTest extends WidgetBase {
+
+  @Test
+  void executeActiveMailingListSubscribers() {
+    addPreferencesFromWidgetXml(widgetContext,
+        "<widget name=\"siteStats\" class=\"stats card\">\n" +
+            "  <title>Active Subscribers</title>\n" +
+            "  <report>active-mailing-list-subscribers</report>\n" +
+            "</widget>");
+    setRoles(widgetContext, ADMIN);
+
+    try (MockedStatic<MailingListMemberRepository> repository = mockStatic(MailingListMemberRepository.class)) {
+      repository.when(MailingListMemberRepository::countActiveSubscribers).thenReturn(280L);
+
+      new SiteStatsWidget().execute(widgetContext);
+    }
+
+    Assertions.assertEquals(SiteStatsWidget.CARD_JSP, widgetContext.getJsp());
+    Assertions.assertEquals("280", request.getAttribute("numberValue"));
+  }
+
+  @Test
+  void executeUnsubscribedMailingListMembers() {
+    addPreferencesFromWidgetXml(widgetContext,
+        "<widget name=\"siteStats\" class=\"stats card\">\n" +
+            "  <title>Unsubscribed</title>\n" +
+            "  <report>unsubscribed-mailing-list-members</report>\n" +
+            "</widget>");
+    setRoles(widgetContext, ADMIN);
+
+    try (MockedStatic<MailingListMemberRepository> repository = mockStatic(MailingListMemberRepository.class)) {
+      repository.when(MailingListMemberRepository::countUnsubscribed).thenReturn(51L);
+
+      new SiteStatsWidget().execute(widgetContext);
+    }
+
+    Assertions.assertEquals(SiteStatsWidget.CARD_JSP, widgetContext.getJsp());
+    Assertions.assertEquals("51", request.getAttribute("numberValue"));
+  }
+
+  @Test
+  void executeMonthlyMailingListSubscriptions() {
+    addPreferencesFromWidgetXml(widgetContext,
+        "<widget name=\"siteStats\" class=\"stats card\">\n" +
+            "  <title>Monthly Mailing List Subscriptions</title>\n" +
+            "  <report>monthly-mailing-list-subscriptions</report>\n" +
+            "  <type>bar</type>\n" +
+            "</widget>");
+    setRoles(widgetContext, ADMIN);
+
+    StatisticsData point = new StatisticsData();
+    point.setLabel("2026-07-01");
+    point.setValue("12");
+
+    try (MockedStatic<MailingListMemberRepository> repository = mockStatic(MailingListMemberRepository.class)) {
+      repository.when(() -> MailingListMemberRepository.findMonthlySubscriptions(anyInt())).thenReturn(List.of(point));
+
+      new SiteStatsWidget().execute(widgetContext);
+
+      repository.verify(() -> MailingListMemberRepository.findMonthlySubscriptions(12));
+    }
+
+    Assertions.assertEquals(SiteStatsWidget.BAR_CHART_JSP, widgetContext.getJsp());
+    Assertions.assertEquals(List.of(point), request.getAttribute("statisticsDataList"));
+  }
+
+  @Test
+  void executeDailyMailingListSubscriptions() {
+    addPreferencesFromWidgetXml(widgetContext,
+        "<widget name=\"siteStats\" class=\"stats card\">\n" +
+            "  <title>Daily Mailing List Subscriptions</title>\n" +
+            "  <report>daily-mailing-list-subscriptions</report>\n" +
+            "</widget>");
+    setRoles(widgetContext, ADMIN);
+
+    try (MockedStatic<MailingListMemberRepository> repository = mockStatic(MailingListMemberRepository.class)) {
+      repository.when(() -> MailingListMemberRepository.findDailySubscriptions(anyInt())).thenReturn(List.of());
+
+      new SiteStatsWidget().execute(widgetContext);
+
+      repository.verify(() -> MailingListMemberRepository.findDailySubscriptions(30));
+    }
+
+    Assertions.assertEquals(SiteStatsWidget.LINE_CHART_JSP, widgetContext.getJsp());
+  }
 
   @Test
   void executeCountOnlineNow() {
